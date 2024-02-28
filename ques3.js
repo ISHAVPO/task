91817b2210044143649b8201716c3b1cf2b7b2f1
ISHAVPO <139744207+ISHAVPO@users.noreply.github.com>
2024-02-28 17:44:44 +0530

    Add files via upload

---
+++ ques3.js
@@ -0,0 +1,15 @@
+var str = ['J', 'Z', 'D', 'Q', 'L', 'W', 'A', 'H', 'Y', 'K', 'G', 'N', 'V', 'R', 'P', 'X', 'C', 'M', 'F', 'O', 'U', 'S', 'I', 'E', 'T', 'B'];
+
+for (var i = 0; i < str.length - 1; i++) {
+    for (var j = 1; i < str.length; j++) {
+
+        if (str[i] > str[j]) {
+            var temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+
+        }
+    }
+    console.log(str)
+}
+console.log(str)
